c4d2f01c8f369d79d7f76d44b4be7fe7ee7ba7f6
shotasan <wondermoai@gmail.com>
2019-06-01 22:13:38 +0900

    Fix: ルーティングエラーを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'devise/sessions#new'
-  resources :users, only: :show
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
 
   # ログイン、アカウント編集後、任意のページに推移させるための記述
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
