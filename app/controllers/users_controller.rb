8bfc693e7738f089762f4775fccfd8506f4ed4cd
shotasan <wondermoai@gmail.com>
2019-06-06 18:54:40 +0900

    Fix: ルーティングエラーを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
-class UsersController < ApplicationController
+class UsersController < Users::RegistrationsController
+  def index
+    redirect_to edit_user_url(current_user)
+  end
+
   def show
-    @user = User.find(params[:id])
+    @user = current_user
   end
 end
