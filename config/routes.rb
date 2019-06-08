4b28a9fcd415bcb10e1a898c57a194c0a41b2762
shotasan <wondermoai@gmail.com>
2019-06-08 17:31:20 +0900

    Add: Reviewsコントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
     resources :users, only: %i[index show]
   end
 
+  resources :reviews
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
