77d103461b2d960d949b24acc2c4d577f2e7228b
shotasan <wondermoai@gmail.com>
2019-06-11 02:42:50 +0900

    Add: Reviewモデルにバリデーションを追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,10 @@ class Review < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :title, :content, :rating, :bitter, :acidity, :rich, :sweet, :aroma, presence: true
+  validates :rating, :bitter, :acidity, :rich, :sweet, :aroma, presence: true
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :content, presence: true, length: { maximum: 1_000 }
+  validates :rating, :bitter, :acidity, :rich, :sweet, :aroma,
+            presence: true,
+            inclusion: { in: %w[1 2 3 4 5] }
 end
