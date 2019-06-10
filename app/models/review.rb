0279995135361de514681c84cccf4ea4eb1c5b5b
shotasan <wondermoai@gmail.com>
2019-06-11 02:11:42 +0900

    Add: Reviewモデルに画像投稿機能を追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   validates :title, :content, :rating, :bitter, :acidity, :rich, :sweet, :aroma, presence: true
 end
