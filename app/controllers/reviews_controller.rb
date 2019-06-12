64c32549908039819e6c1801ff82e3d2219c1c72
shotasan <wondermoai@gmail.com>
2019-06-12 22:02:48 +0900

    Add: Reviewコントローラを修正し、BeanとTargetを同時に保存する機能を追加

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,6 +8,8 @@ class ReviewsController < ApplicationController
 
   def new
     @review = current_user.reviews.build
+    @bean = Bean.new
+    @target = Target.new
   end
 
   def edit
@@ -15,9 +17,17 @@ class ReviewsController < ApplicationController
 
   def create
     @review = current_user.reviews.build(review_params)
+    @bean = Bean.new(bean_params)
+    @target = Target.new(target_params)
 
-    if @review.save
-      redirect_to review_url(@review), notice: '登録に成功しました。'
+    if @review.save && @bean.save
+      @target.review_id = @review.id
+      @target.bean_id = @bean.id
+      if @target.save
+        redirect_to review_url(@review), notice: '登録に成功しました。'
+      else
+        render :new
+      end
     else
       render :new
     end
@@ -33,5 +43,13 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:original?, :title, :content, :image, :drank_on, :rating, :bitter, :acidity, :rich, :sweet, :aroma)
+  end
+
+  def bean_params
+    params.require(:bean).permit(:name, :country, :plantation)
+  end
+
+  def target_params
+    params.require(:target).permit(:roasted, :roasted_on, :grind, :amount)
   end
 end
