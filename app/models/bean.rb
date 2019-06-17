9fd0233c267bd641506c6dea07294c5741f9b015
shotasan <wondermoai@gmail.com>
2019-06-17 19:55:07 +0900

    Add: Beanモデルにバリデーションを追加

--- app/models/bean.rb
+++ app/models/bean.rb
@@ -1,3 +1,7 @@
 class Bean < ApplicationRecord
   has_many :targets, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :country, length: { maximum: 30 }
+  validates :plantation, length: { maximum: 30 }
 end
