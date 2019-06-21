b7876aa1cd3f88dccd2702a8dcffb8067f0bd17d
shotasan <wondermoai@gmail.com>
2019-06-21 18:06:00 +0900

    Fix: targetモデルのamountが0以上100以下になるようバリデーションと入力欄を修正

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,4 +1,7 @@
 class Target < ApplicationRecord
   belongs_to :review
   belongs_to :bean
+
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+
 end
