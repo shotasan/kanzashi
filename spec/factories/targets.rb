574685761dfc1ee5791565548bba5cf9ca5f72d0
shotasan <wondermoai@gmail.com>
2019-06-21 18:07:13 +0900

    Test: targetモデルのModelSpecをバリデーションの修正に合わせて修正

--- spec/factories/targets.rb
+++ spec/factories/targets.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :target do
     review { nil }
     bean { nil }
-    store { nil }
     roasted { "MyString" }
     roasted_on { "2019-06-11" }
     grind { "MyString" }
--- spec/models/target_spec.rb
+++ spec/models/target_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Target, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '新規登録のテスト' do
+    let(:target){ FactoryBot.build(:target) }
+    context 'バリデーションのテスト' do
+      it 'amountが0未満なら無効な状態であること' do
+        target.amount = -1
+        target.valid?
+        # binding.pry
+        expect(target.errors[:amount]).to include('は0以上の値にしてください')
+      end
+
+      it 'amountが100より多いなら無効な状態であること' do
+        target.amount = 101
+        target.valid?
+        expect(target.errors[:amount]).to include('は100以下の値にしてください')
+      end
+    end
+  end
 end
