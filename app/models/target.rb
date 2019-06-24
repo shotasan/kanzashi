729dfa534783e381039411aa97ca83ced1626194
shotasan <wondermoai@gmail.com>
2019-06-24 13:39:27 +0900

    Add: Targetモデルに自身が登録したBean以外を選択できないようバリデーションを追加

--- app/models/target.rb
+++ app/models/target.rb
@@ -3,5 +3,13 @@ class Target < ApplicationRecord
   belongs_to :bean
 
   validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+  validate :bean_registered_by_current_user?
 
+  private
+
+  def bean_registered_by_current_user?
+    unless review.user.beans.any? {|bean| bean.id == bean_id}
+      errors.messages[:bean_id] << 'は登録されていません'
+    end
+  end
 end
