0b1b45423b997e279b7ea188e6a572c5912c35b3
shotasan <wondermoai@gmail.com>
2019-06-28 18:16:20 +0900

    Add; reviewの新規登録時に画像が選択されて無ければ、デフォルト画像が選択される機能を追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :user
   has_many :targets, dependent: :destroy
-  accepts_nested_attributes_for :targets
+  accepts_nested_attributes_for :targets, allow_destroy: true
   has_one_attached :image
+
+  before_create :image_nil
 
   validates :rating, :bitter, :acidity, :rich, :sweet, :aroma, presence: true
   validates :title, presence: true, length: { maximum: 30 }
@@ -10,4 +12,12 @@ class Review < ApplicationRecord
   validates :rating, :bitter, :acidity, :rich, :sweet, :aroma,
             presence: true,
             inclusion: { in: [*1..5] }
+
+  private
+
+  def image_nil
+    unless self.image.attached?
+      self.image.attach(io: File.open('app/assets/images/no_image.jpg'), filename: 'no_image.jpg', content_type: 'image/jpg')
+    end
+  end
 end
