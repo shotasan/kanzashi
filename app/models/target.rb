0546b32909e164db42cf2f4bc0b29d2953972d09
shotasan <wondermoai@gmail.com>
2019-06-29 16:04:26 +0900

    Fix: reviewの新規作成画面で焙煎方法と挽き方を空欄にすると登録されないエラーを修正

--- app/models/target.rb
+++ app/models/target.rb
@@ -3,8 +3,8 @@ class Target < ApplicationRecord
   belongs_to :bean
 
   validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
-  validates :roasted, inclusion: { in: ApplicationController.helpers.roasted_select_box }
-  validates :grind, inclusion: { in: ApplicationController.helpers.grind_select_box }
+  validates :roasted, inclusion: { in: ApplicationController.helpers.roasted_select_box }, allow_blank: true
+  validates :grind, inclusion: { in: ApplicationController.helpers.grind_select_box }, allow_blank: true
   validate :bean_registered_by_current_user?
 
   private
