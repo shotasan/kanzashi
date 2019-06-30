b89f8435931aa359b2a1487f3ca31b5cb6517977
shotasan <wondermoai@gmail.com>
2019-06-30 17:20:33 +0900

    Add: targetのroasted_onに未来の日付が入力されないようバリデーションを追加

--- app/models/target.rb
+++ app/models/target.rb
@@ -6,12 +6,19 @@ class Target < ApplicationRecord
   validates :roasted, inclusion: { in: ApplicationController.helpers.roasted_select_box }, allow_blank: true
   validates :grind, inclusion: { in: ApplicationController.helpers.grind_select_box }, allow_blank: true
   validate :bean_registered_by_current_user?
+  validate :future_date_prohibited
 
   private
 
   def bean_registered_by_current_user?
     unless review.user.beans.any? {|bean| bean.id == bean_id}
       errors.messages[:bean_id] << 'は登録されていません'
+    end
+  end
+
+  def future_date_prohibited
+    if roasted_on.present? && roasted_on > Date.today
+      errors.add(:roasted_on, "に未来の日付は入力できません")
     end
   end
 end
