1e2e136c6717448a4af8a8f52c01930a956daff6
shotasan <wondermoai@gmail.com>
2019-07-01 10:49:42 +0900

    Add: reviewに削除機能を追加

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: %i[edit update destroy]
+
   def index
     @reviews = Review.all
   end
@@ -16,9 +18,7 @@ class ReviewsController < ApplicationController
     end
   end
 
-  def edit
-    @review = current_user.reviews.find(params[:id])
-  end
+  def edit; end
 
   def create
     @review = current_user.reviews.build(review_params)
@@ -31,19 +31,23 @@ class ReviewsController < ApplicationController
   end
 
   def update
-    @review = current_user.reviews.find(params[:id])
-
     if @review.update(review_params)
-      redirect_to review_path(@review), notice: '編集に成功しました。'
+      redirect_to review_url(@review), notice: '編集に成功しました。'
     else
       render :edit
     end
   end
 
   def destroy
+    @review.destroy
+    redirect_to reviews_url, notice: "#{ @review.title }を削除しました。"
   end
 
   private
+
+  def set_review
+    @review = current_user.reviews.find(params[:id])
+  end
 
   def review_params
     params.require(:review).permit(:original?, :title, :content, :image, :drank_on, :rating, :bitter, :acidity, :rich, :sweet, :aroma,
