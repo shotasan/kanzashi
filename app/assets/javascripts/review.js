6de9b5f8b772032cdbf63369e313f412aaeef3ff
shotasan <wondermoai@gmail.com>
2019-07-02 15:57:27 +0900

    Add: reviewで画像を登録する際にプレビューが表示される機能を追加

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -18,4 +18,35 @@ $(document).on('ready turbolinks:load',(function() {
     })
 }));
 
+// 画像のプレビュー機能
+$(document).on('ready turbolinks:load',(function() {
+    $(function () {
+        $('.image_file').change(function () {
+            // 画像の情報を取得
+            var file = this.files[0];
+            var img_tag_id = $(this).data('imageTagId');
+
+            // 指定の拡張子以外の場合はアラート
+            var permit_type = ['image/jpeg', 'image/png', 'image/gif', 'image/jpg'];
+            if (file && permit_type.indexOf(file.type) == -1) {
+                alert('この形式のファイルはアップロードできません');
+                $(this).val('');
+                $(img_tag_id).attr('src', '');
+                return
+            }
+
+            // 読み込んだ画像を取得し、フォームの直後に表示させる
+            var reader = new FileReader();
+            reader.onload = function () {
+                $(img_tag_id).attr('src', reader.result);
+                $(img_tag_id + '_prev').attr('src', '');
+            };
+
+            // 画像の読み込み
+            reader.readAsDataURL(file);
+
+            $('#preview-image').show()
+        });
+
+    });
 }));
