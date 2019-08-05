1c064dff19d8f0b710841b5c4c6f14dba51fac77
shotasan <wondermoai@gmail.com>
2019-08-05 19:45:16 +0900

    Add: beanを削除した際に関連するレビューが削除される機能を追加

--- app/models/bean.rb
+++ app/models/bean.rb
@@ -9,4 +9,17 @@ class Bean < ApplicationRecord
   def beans_select_box
     "名称: #{self.name} 原産国: #{self.country} 農園: #{self.plantation}"
   end
+
+  # bean削除時に関連するレビューを削除するメソッド
+  def self.destroy_bean_and_related_reviews(bean)
+    bean.transaction do
+      review_ids = Target.where(bean_id: bean).pluck(:review_id)
+      related_reviews = Review.where(id: review_ids)
+      related_reviews.destroy_all
+      bean.destroy!
+    end
+  rescue StandardError => e
+    logger.debug(e)
+    false
+  end
 end
