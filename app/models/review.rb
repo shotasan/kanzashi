fb439622c8a7d8a28dec06911fd613a0b3f36306
shotasan <wondermoai@gmail.com>
2019-08-09 01:56:27 +0900

    Add: reviewモデルにtargetモデルとの関連付けに関するバリデーションを追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,6 +8,7 @@ class Review < ApplicationRecord
 
   before_create :image_nil, :check_straight_or_blend
 
+  validates :targets, presence: true
   validates :rating, :bitter, :acidity, :rich, :sweet, :aroma, presence: true
   validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 1_000 }
