c7e60882803a6218f89058da4b8c9d7ef3365893
shotasan <wondermoai@gmail.com>
2019-08-14 01:24:31 +0900

    Update: ログファイルの設定を修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.logger = Logger.new('log/development.log', 'daily')
+  # config.logger = Logger.new('log/development.log', 'daily')
 end
