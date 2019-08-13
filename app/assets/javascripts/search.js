ee1299773399b78e04aa6312e0dcfb5b3f48a1bc
shotasan <wondermoai@gmail.com>
2019-08-14 01:42:49 +0900

    Fix: 検索フォームの星の数が増えるエラーを修正

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,7 +2,9 @@ $(document).on('turbolinks:load',(function() {
     let rating_items = ['rating', 'bitter', 'acidity', 'rich', 'sweet', 'aroma'];
 
     rating_items.forEach(function(value) {
-        $(`#${value}-search`).raty({
+        $(`#${ value }-search`)
+            .empty()
+            .raty({
             path: '/assets/',
             score: 1,
             scoreName: `q[${value}_gteq]`
