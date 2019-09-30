03c79389d0146baae15fe75f2986c153db53e90b
shotasan <wondermoai@gmail.com>
2019-09-30 16:04:00 +0900

    Add: 画像のURLを取得するヘルパーを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def current_user?(klass)
     klass.user == current_user
   end
+
+  def object_url(object_image)
+    if Rails.env.test?
+      object_image
+    else
+      object_image.attachment.service.send(:object_for, object_image.key).public_url
+    end
+  end
 end
