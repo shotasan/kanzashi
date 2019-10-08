b49bcccd304dff8ab4cf80ecd3f07da55b9761ad
shotasan <wondermoai@gmail.com>
2019-10-08 22:41:11 +0900

    Fix: pumaの設定ファイルを修正

--- config/puma.rb
+++ config/puma.rb
@@ -39,8 +39,10 @@ end
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-app_dir = File.expand_path("../..", __FILE__)
-bind "unix://#{app_dir}/tmp/sockets/puma.sock"
-pidfile "#{app_dir}/tmp/pids/puma.pid"
-state_path "#{app_dir}/tmp/pids/puma.state"
-stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+if Rails.env.production?
+  app_dir = File.expand_path("../..", __FILE__)
+  bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+  pidfile "#{app_dir}/tmp/pids/puma.pid"
+  state_path "#{app_dir}/tmp/pids/puma.state"
+  stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+end
