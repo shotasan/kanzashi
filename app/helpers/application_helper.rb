0e2e749d35ab3bf8854e4f143638e81c1820a772
shotasan <wondermoai@gmail.com>
2019-10-10 23:16:39 +0900

    Add: ヘルパーの記載を修正

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,32 @@
+module ApplicationHelper
+  def current_user?(klass)
+    klass.user == current_user
+  end
+
+  # production環境でS3オブジェクトのURLを取得するためのメソッド
+  def object_url(object_image)
+    if Rails.env.production?
+      object_image.attachment.service.send(:object_for, object_image.key).public_url
+    else
+      url_for(object_image)
+    end
+  end
+
+  # ヘッダーアイコンのリンク先をログイン前後で変更するため
+  def header_icon_url
+    user_signed_in? ? reviews_path : root_path
+  end
+
+  def image_attached?(object)
+    if object.user.avatar.attached?
+      ActionController::Base.helpers.image_tag(object_url(object.user.avatar))
+    else
+      ActionController::Base.helpers.image_tag('avatar_no_image.jpg')
+    end
+  end
+
+  # 必須入力項目に表示されるバッジ
+  def requier_badge
+    content_tag :span, '必須', class: 'badge badge-danger ml-1'
+  end
+end
