15d6306ac0dfea4d47a4b082422b123eeaa8be3e
shotasan <wondermoai@gmail.com>
2019-10-30 11:42:23 +0900

    Add: ゲストユーザー機能を追加

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,12 @@ module UsersHelper
   def avatar_present?
     current_user.persisted? && current_user.avatar.attached?
   end
+
+  def guest_email
+    'guest@sample.com'
+  end
+
+  def guest_password
+    'guestuser'
+  end
 end
