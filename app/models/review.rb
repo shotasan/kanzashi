020a91350a6e1a772d16842d9acf0c8dfc00014a
shotasan <wondermoai@gmail.com>
2019-11-07 02:20:24 +0900

    Add: reviewモデルにitemのバリデーションを追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -10,6 +10,7 @@ class Review < ApplicationRecord
   validates :rating, :bitter, :acidity, :rich, :sweet, :aroma, presence: true
   validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 1_000 }
+  validates :item, length: { maximum: 100 }
   validates :rating, :bitter, :acidity, :rich, :sweet, :aroma,
             presence: true,
             inclusion: { in: [*1..5] }
